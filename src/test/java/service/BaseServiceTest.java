8c897035c54aec1df364ad53acaccda961380f21
qingyang <qingyang@wacai.com>
2017-10-14 18:44:52 +0800

    unitils 与spring 集成

--- src/test/java/service/BaseServiceTest.java
+++ src/test/java/service/BaseServiceTest.java
@@ -1,7 +1,5 @@
 package service;
 
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
 import org.unitils.UnitilsJUnit4;
 import org.unitils.database.annotations.Transactional;
 import org.unitils.database.util.TransactionMode;
@@ -11,8 +9,7 @@ import org.unitils.spring.annotation.SpringApplicationContext;
  * Created by yangjianzhou on 16-4-16.
  */
 
-@Transactional(value = TransactionMode.COMMIT)
-//@ContextConfiguration({"classpath:applicationContext-test.xml", "classpath:dataSource-test.xml"})
+@Transactional(value = TransactionMode.ROLLBACK)
 @SpringApplicationContext({"classpath:applicationContext-test.xml", "classpath:dataSource-test.xml"})
 public abstract class BaseServiceTest extends UnitilsJUnit4 {
 }
--- src/test/java/service/ProductServiceTest.java
+++ src/test/java/service/ProductServiceTest.java
@@ -3,6 +3,7 @@ package service;
 import com.yangjianzhou.dto.ProductDTO;
 import com.yangjianzhou.service.ProductService;
 import org.junit.Assert;
+import org.junit.Test;
 import org.unitils.dbunit.annotation.DataSet;
 import org.unitils.spring.annotation.SpringBeanByType;
 
@@ -14,10 +15,9 @@ public class ProductServiceTest extends BaseServiceTest {
     @SpringBeanByType
     private ProductService productService;
 
-    @org.junit.Test
+    @Test
     @DataSet({"/data/tb_product.xls"})
     public void test_insert() {
-
         ProductDTO productDTO = productService.queryProductById(4L);
         Assert.assertTrue("zhangsan".equals(productDTO.getName()));
     }
